c745d7be406ec45222c870c5bd5cb322549176f9
Rohit Baba <rohitbaba198@gmail.com>
2024-11-29 19:05:51 +0530

    changes in vit.config.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default {
+export default defineConfig({
   server: {
-    host: "0.0.0.0", // Listen on all interfaces
-    port: 5173, // You can use a custom port or rely on the default
+    host: "0.0.0.0", // Listen on all network interfaces (for deployment)
+    port: 5173, // Use the default port or choose your custom port
   },
-};
+  plugins: [react()],
+});
